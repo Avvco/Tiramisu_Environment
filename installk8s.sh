105eef43a596ac5d92eddd8f7546c9d299fae93c
Avvco <leei910110@gmail.com>
2022-06-23 15:55:08 +0800

    add installk8s but not in use

---
+++ installk8s.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install k8s
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# =============================================
+
+# Install minicube
+# https://minikube.sigs.k8s.io/docs/start/
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# set default driver to docker
+minikube config set driver docker
+
+# Add your user to the 'docker' group
+sudo usermod -aG docker $USER && newgrp docker
