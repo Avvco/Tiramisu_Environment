1cf87f86f092b41290506cd79b06fd70bfa6313c
Avvco <leei910110@gmail.com>
2022-10-20 15:12:11 +0800

    update java version to parameter

--- installTools.sh
+++ installTools.sh
@@ -3,16 +3,18 @@ apt install tmux
 apt install net-tools --fix-missing
 
 
-# Install jdk18
+# Install jdk
+JAVA_VERSION="19"
+
 apt-get update
 apt-get install -y wget
 apt install -y libc6-x32 libc6-i386
-wget https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.deb
-apt --fix-broken install -y ./jdk-18_linux-x64_bin.deb
-rm -f ./jdk-18_linux-x64_bin.deb
+wget https://download.oracle.com/java/${JAVA_VERSION}/latest/jdk-${JAVA_VERSION}_linux-x64_bin.deb
+apt --fix-broken install -y ./jdk-${JAVA_VERSION}_linux-x64_bin.deb
+rm -f ./jdk-${JAVA_VERSION}_linux-x64_bin.deb
 
 # Set environment variable
-JAVA_HOME="/usr/lib/jvm/jdk-18"
+JAVA_HOME="/usr/lib/jvm/jdk-${JAVA_VERSION}"
 rm -f /etc/profile.d/java_env.sh
-echo JAVA_HOME=$JAVA_HOME > /etc/profile.d/java_env.sh
-echo PATH=${PATH}:$JAVA_HOME/bin >> /etc/profile.d/java_env.sh
+echo JAVA_HOME=${JAVA_HOME} > /etc/profile.d/java_env.sh
+echo PATH=${PATH}:${JAVA_HOME}/bin >> /etc/profile.d/java_env.sh
